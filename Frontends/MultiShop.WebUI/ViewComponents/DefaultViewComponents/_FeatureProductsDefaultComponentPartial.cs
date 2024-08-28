8ed6e179b25b972d455c97bf953e85e1eb3e51a6
Hasan Akcan <hasanakcann@gmail.com>
2024-08-28 23:09:30 +0300

    feature product api and ui development

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+using Newtonsoft.Json;
 
-namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
+namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents;
+
+public class _FeatureProductsDefaultComponentPartial : ViewComponent
 {
-    public class _FeatureProductsDefaultComponentPartial : ViewComponent
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _FeatureProductsDefaultComponentPartial(IHttpClientFactory httpClientFactory)
     {
-        public IViewComponentResult Invoke()
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7070/api/Products");
+
+        if (responseMessage.IsSuccessStatusCode)
         {
-            return View();
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+            return View(values);
         }
+
+        return View();
     }
 }
